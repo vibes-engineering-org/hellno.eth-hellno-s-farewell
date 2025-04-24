b4fe701aec9a2e7d545b8be1e9476c32ac2db7ec
hellno <hellno@users.noreply.github.com>
2025-04-25 00:35:43 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const frame = {
   version: "next",
   imageUrl: `${appUrl}/opengraph-image`,
   button: {
-    title: "Launch Frame",
+    title: "help hellno.eth",
     action: {
       type: "launch_frame",
       name: PROJECT_TITLE,
