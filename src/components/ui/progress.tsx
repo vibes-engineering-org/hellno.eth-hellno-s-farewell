cf2f72475fd5256f7ce8237ac2d12af46bcbc82a
hellno <hellno@users.noreply.github.com>
2025-04-24 23:23:24 +0200

    Update progress.tsx

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,25 +1,28 @@
-"use client";
+"use client"
 
-import * as React from "react";
-import * as ProgressPrimitive from "@radix-ui/react-progress";
-import { cn } from "../../lib/utils";
+import * as React from "react"
+import * as ProgressPrimitive from "@radix-ui/react-progress"
 
-export interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {}
+import { cn } from "~/lib/utils"
 
-export const Progress = React.forwardRef<React.ElementRef<typeof ProgressPrimitive.Root>, ProgressProps>(
-  ({ className, value, ...props }, ref) => (
-    <ProgressPrimitive.Root
-      ref={ref}
-      value={value}
-      className={cn("relative h-2 w-full overflow-hidden rounded-full bg-gray-200", className)}
-      {...props}
-    >
-      <ProgressPrimitive.Indicator
-        className="h-full bg-primary transition-all"
-        style={{ transform: `translateX(-${100 - (value ?? 0)}%)` }}
-      />
-    </ProgressPrimitive.Root>
-  )
-);
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+>(({ className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-4 w-full overflow-hidden rounded-full bg-neutral-100 dark:bg-neutral-800",
+      className
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className="h-full w-full flex-1 bg-neutral-900 transition-all dark:bg-neutral-50"
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
+))
+Progress.displayName = ProgressPrimitive.Root.displayName
 
-Progress.displayName = ProgressPrimitive.Root.displayName;
+export { Progress }
