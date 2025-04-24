b607d6b76d6ea17c2183ab256ed86f6d9f634cd1
hellno <hellno@users.noreply.github.com>
2025-04-24 22:16:15 +0200

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const PROJECT_ID = "hellno-europoor-to-nyc";
-export const PROJECT_TITLE = "Hellno's Europoor to NYC";
-export const PROJECT_DESCRIPTION = "Fundraising to support Hellno's journey from Europoor to NYC";
+export const PROJECT_TITLE = "hellno's europoor journey to farcon nyc";
+export const PROJECT_DESCRIPTION = "Fundraising to support hellno's journey from the land of Europooors to Farcon NYC";
