1cd227ba5e7a8bcc1b4db99ab6895402c2fe90eb
hellno <hellno@users.noreply.github.com>
2025-04-28 12:32:21 +0200

    add more frens

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,55 @@
 export const supportedBy = [
   {
+    username: "jachian",
+    pfpUrl:
+      "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/e27518c4-686e-429a-92f1-f635c05a1100/anim=false,fit=contain,f=auto,w=288",
+  },
+  {
+    username: "linda",
+    pfpUrl:
+      "https://wrpcd.net/cdn-cgi/image/anim=false,fit=contain,f=auto,w=288/https://i.seadn.io/gae/r6CW_kgQygQhI7-4JdWt_Nbf_bjFNnEM7dSns1nZGrijJvUMaLnpAFuBLwjsHXTkyX8zfgpRJCYibtm7ojeA2_ASQwSJgh7yKEFVMOI?w=500&auto=format",
+  },
+  {
+    username: "stephancill",
+    pfpUrl:
+      "https://wrpcd.net/cdn-cgi/image/anim=false,fit=contain,f=auto,w=336/https%3A%2F%2Fimages.colorino.site%2F19c754968a5a0d6e5b554a3aa8107d5fe973a88550080f7b1713e3c33989ffa0.png",
+  },
+  {
+    username: "timdaub",
+    pfpUrl:
+      "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/1cfcb87c-e5d3-4cc4-e0a5-d02d38409b00/anim=false,fit=contain,f=auto,w=336",
+  },
+  {
+    username: "m-j-r",
+    pfpUrl:
+      "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/8baf8323-29a5-4c4e-8531-36a3d6894c00/anim=false,fit=contain,f=auto,w=288",
+  },
+  {
+    username: "horsefacts",
+    pfpUrl:
+      "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/27ebb092-6f26-4397-6027-8c90d909ce00/anim=false,fit=contain,f=auto,w=288",
+  },
+  {
+    username: "jrf",
+    pfpUrl:
+      "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/7917ea6d-3745-4cb1-c5cc-86bd4c23e100/anim=false,fit=contain,f=auto,w=288",
+  },
+  {
+    username: "francos",
+    pfpUrl:
+      "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/4f849cc3-4b33-41df-987e-e419d246c600/anim=false,fit=contain,f=auto,w=288",
+  },
+  {
+    username: "hinhuk",
+    pfpUrl:
+      "https://wrpcd.net/cdn-cgi/image/anim=false,fit=contain,f=auto,w=288/https://i.imgur.com/Nwmks8z.jpg",
+  },
+  {
+    username: "runninyeti",
+    pfpUrl:
+      "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/72bc3f94-fa12-4dab-f050-5dd6a7b26400/anim=false,fit=contain,f=auto,w=288",
+  },
+  {
     username: "biji",
     pfpUrl:
       "https://wrpcd.net/cdn-cgi/image/anim=false,fit=contain,f=auto,w=288/https%3A%2F%2Fi.imgur.com%2Fzwxa0tc.jpg",
@@ -15,11 +65,6 @@ export const supportedBy = [
       "https://wrpcd.net/cdn-cgi/image/anim=false,fit=contain,f=auto,w=288/https%3A%2F%2Fi.imgur.com%2FBupKd3h.jpg",
   },
   {
-    username: "timdaub",
-    pfpUrl:
-      "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/1cfcb87c-e5d3-4cc4-e0a5-d02d38409b00/anim=false,fit=contain,f=auto,w=336",
-  },
-  {
     username: "nt",
     pfpUrl:
       "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/48fcaae2-ad08-4d24-4317-2094d979b800/anim=false,fit=contain,f=auto,w=336",
@@ -28,11 +73,6 @@ export const supportedBy = [
     username: "harris-",
     pfpUrl:
       "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/ea8cac53-6cbf-429e-3be6-d5923cf59200/anim=false,fit=contain,f=auto,w=336",
-  },
-  {
-    username: "stephancill",
-    pfpUrl:
-      "https://wrpcd.net/cdn-cgi/image/anim=false,fit=contain,f=auto,w=336/https%3A%2F%2Fimages.colorino.site%2F19c754968a5a0d6e5b554a3aa8107d5fe973a88550080f7b1713e3c33989ffa0.png",
   },
   {
     username: "kompreni",
